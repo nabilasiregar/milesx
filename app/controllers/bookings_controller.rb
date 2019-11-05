99b79af99f7434b91c9fca0121ce5aee2f954304
Jielun Chen <chenjielunn@gmail.com>
2019-11-05 18:22:28 +0800

    partial bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,16 @@
 class BookingsController < ApplicationController
+before_action :authenticate_user!, only: [:create]
+
   def index
     @bookings = Booking.all
   end
 
   def show
     @booking = Booking.find(params[:id])
-    @past_bookings = Booking.where("end_date < ?", "#{Date.today}")
+    @past_bookings = Booking.where("end_date < ?", Date.today)
   end
 
   def create
-    if user_signed_in?
-    if current_user.present?
         @booking = Booking.new(booking_params)
     if @booking.save
         direct_to booking_path(@booking)
