dd0c42ec0707fdd66ba6ecc63a014f8c2e48730f
Jielun Chen <chenjielunn@gmail.com>
2019-11-05 17:22:42 +0800

    booking controller added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,23 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all?
+    @bookings = Booking.all
   end
 
   def show
+    @booking = Booking.find(params[:id])
+    @past_bookings = Booking.where("end_date < ?", "#{Date.today}")
   end
 
   def create
+    if user_signed_in?
+    if current_user.present?
+        @booking = Booking.new(booking_params)
+    if @booking.save
+        direct_to booking_path(@booking)
+        flash[:notice] = 'Your flight has been booked!'
+    else
+        render 'bookings/show'
+    end
   end
 
-  def new
-  end
 end
