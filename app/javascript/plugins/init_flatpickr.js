06a358048417c905ec1fc90f3d95cf720e624121
Jielun Chen <chenjielunn@gmail.com>
2019-11-08 09:48:24 +0800

    added flatpickr for user to select birthday on bookings page

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -3,8 +3,10 @@ import flatpickr from "flatpickr";
 const initFlatpickr = () => {
   const departureDateInput = document.getElementById('depart-date');
   const returnInput = document.getElementById('return-date');
+  const birthDayInput = document.getElementById('birth-date');
   departureDateInput && flatpickr(departureDateInput)
   returnInput && flatpickr(returnInput)
+  birthDayInput && flatpickr(birthDayInput)
 }
 
 
